4e6ec5e7d3d63aef998943795ca90b367452ab21
Howard <nnt18119@hotmail.com>
2017-07-28 14:15:30 +0800

    first command

---
+++ README.md
@@ -0,0 +1,8 @@
+# word frequency analysis
+
+## 執行方式
+
+將要分析的檔案放在update資料夾底下, 檔名須為text.txt
+`npm install` download 需要用到的plugin
+
+`npm start` 啟動 local web server
---
+++ server.js
@@ -0,0 +1,68 @@
+// ref - https://stackoverflow.com/questions/6831918/node-js-read-a-text-file-into-an-array-each-line-an-item-in-the-array
+var fs = require('fs');
+var dic = require('./dictionary/dictionary.json');
+
+fs.readFile('update/text.txt', 'utf8', function(err, data) {
+    if(err) throw err;
+    console.log(dic.AND);
+    var array = data.toString().split("\n");
+    array.sort();
+    var newText = "",
+        prev, 
+        freq = 1;
+    for(i in array) {
+        if(array[i] == ""){
+            continue;
+        }
+        // check for one word per line.
+        arr_words = array[i].split(/\s+/);
+        if( array[i].split(/\s+/).length > 1){
+            console.error("Error: please one word per line.");
+            // return console.error("at " + ++i + " line");
+            newTextFile(array);
+            return false;
+        }
+        // check for dicrtionary.
+        if(dic[array[i].toUpperCase()] == undefined){
+            console.error("Error: can not find the word- " + array[i] + " at dictionary");
+            // console.error("at " + i + " line");
+            return false
+        }
+
+        if ( array[i] !== prev ) {
+            newText += freq + " - " + array[i] + "\n";
+            freq = 1;
+        } else {
+            freq++;
+        }
+        prev = array[i];
+
+        // console.log(i + "-"+array[i]);
+    }
+    writeTextFile(newText, "finText");
+    
+});
+
+function newTextFile(array){
+    var newText = "", arr_words = 0;
+    for(i in array) {
+        arr_words = array[i].split(" ");
+        if( arr_words.length > 1){
+            for( j in arr_words){
+                newText += arr_words[j] + "\n";
+            }
+        } else {
+            newText += array[i] + "\n";
+        }
+    }
+    writeTextFile( newText, "newText")
+    
+}
+function writeTextFile(text, name){
+    fs.writeFile("update/" + name + ".txt", text, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+        console.log("create a new file at update/"+ name + ".txt");
+    }); 
+}
